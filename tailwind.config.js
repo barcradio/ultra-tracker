515b02d54fc6ede00d43013beda2252cfa900d13
Jaren Glenn <jarenglenn@gmail.com>
2024-05-23 04:08:54 +0100

    build: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/renderer/index.html",
+    "./src/renderer/**/*.{ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
