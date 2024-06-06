8dcc826fe3c8d274f3ce5a197fb2676e268b59a4
Jaren Glenn <jarenglenn@gmail.com>
2024-06-05 23:58:20 -0400

    fix: fix clock width changing each second

--- src/renderer/src/features/Header/Clock.tsx
+++ src/renderer/src/features/Header/Clock.tsx
@@ -5,5 +5,9 @@ export function Clock() {
   const [currentTime] = useCurrentTime();
   const formatted = format(currentTime, "kk:mm:ss a");
 
-  return <h1 className="p-6 font-display text-5xl font-semibold text-yellow-400">{formatted}</h1>;
+  return (
+    <h1 className="min-w-80 p-6 font-display text-5xl font-semibold text-yellow-400">
+      {formatted}
+    </h1>
+  );
 }
--- src/renderer/src/features/Header/Header.tsx
+++ src/renderer/src/features/Header/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     <div className="flex flex-row items-center justify-between bg-slate-800 p-4 text-slate-100">
       <Clock />
-      <h1 className="font-display text-2xl font-bold">Ultra Tracker</h1>
+      <h1 className="font-display text-3xl font-bold">Ultra Tracker</h1>
       <BibForm />
     </div>
   );
