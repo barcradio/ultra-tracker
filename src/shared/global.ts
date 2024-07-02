5a236452f3473e73f4eeb22c150630d8fc79c63b
Russ Leikis <rleikis@gmail.com>
2024-07-01 18:17:29 -0600

    feat: added shared/global properties and object types

---
+++ src/shared/global.ts
@@ -0,0 +1,7 @@
+
+global.shared = {
+    targetLanguage: 'eng',
+    dbPath: "./Database",
+    dbFullPath: "./Database/Bear100Devdb.db"
+  };
+  export default global;
---
+++ src/shared/models.ts
@@ -0,0 +1,35 @@
+export type Runner = {
+    index: number
+    bib: number
+    firstname: string
+    lastname: string
+    gender: string
+    age: number
+    city: string
+    state: string
+    emPhone: string
+    emName: string
+    dns: Boolean
+    dnf: Boolean
+    dnfStation: number
+    dnfDateTime: Date
+}
+
+export type TimingRecord = {
+    bib: number
+    datetime: Date
+    in: Boolean
+    out: Boolean
+    note: string
+}
+
+export type Station = {
+    name: string
+    operators: Operator[]
+}
+
+export type Operator = {
+    fullname: string
+    callsign: string
+    phone: number
+}
