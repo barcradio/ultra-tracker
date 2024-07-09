1f9b3f9b10e5b508c77065e7af446e2d0f2321c9
Jaren Glenn <jarenglenn@gmail.com>
2024-07-09 15:10:32 -0400

    refactor(sidebar): simplify styling of sidebar button

--- src/renderer/src/features/Sidebar/SidebarButton.tsx
+++ src/renderer/src/features/Sidebar/SidebarButton.tsx
@@ -1,6 +1,5 @@
 import { FunctionComponent, SVGProps } from "react";
 import { classed } from "@tw-classed/react";
-import { cva } from "class-variance-authority";
 import { Stack } from "~/components";
 
 export interface SidebarItemProps {
@@ -12,38 +11,29 @@ export interface SidebarButtonProps extends SidebarItemProps {
   active?: boolean;
   onClick?: () => void;
 }
-
-const IconClassName = cva("mr-3 ml-1", {
-  variants: {
-    active: {
-      true: "fill-primary",
-      false: "fill-on-surface-low"
-    }
-  },
-  defaultVariants: {
-    active: false
-  }
-});
 //
-const SidebarStack = classed(Stack, "py-2 text-lg font-bold uppercase text-on-surface-low", {
-  variants: {
-    active: {
-      true: "border-r-4 border-primary text-primary",
-      false:
-        "hover:border-r-4 *:hover:fill-on-surface hover:border-on-surface-low hover:text-on-surface"
+const SidebarStack = classed(
+  Stack,
+  "py-2 my-0.5 ml-4 text-lg font-bold uppercase border-r-4 border-transparent transition-all duration-100 *:transition-all *:duration-100 text-on-surface-low [&>svg]:mr-4 [&>svg]:ml-1",
+  {
+    variants: {
+      active: {
+        true: "border-r-4 border-primary text-primary [&>svg]:fill-primary",
+        false:
+          "hover:border-r-4 *:hover:fill-on-surface [&>svg]:fill-on-surface-low hover:border-on-surface-low hover:text-on-surface"
+      }
+    },
+    defaultVariants: {
+      active: false
     }
-  },
-  defaultVariants: {
-    active: false
   }
-});
+);
 
 export function SidebarButton(props: SidebarButtonProps) {
   return (
     <button className="w-full" type="button" onClick={props.onClick}>
-      <SidebarStack direction="row" align="center" className="my-0.5 ml-4" active={props.active}>
+      <SidebarStack direction="row" align="center" active={props.active}>
         {props.icon({
-          className: IconClassName({ active: props.active }),
           title: props.children,
           height: 28,
           width: 28
