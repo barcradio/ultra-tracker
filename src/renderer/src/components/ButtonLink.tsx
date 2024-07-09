46534074fa91b124e738648b531c907f35feb7b9
Jaren Glenn <jarenglenn@gmail.com>
2024-07-09 00:12:13 -0400

    fix: fix type of ButtonLink's `to` prop

--- src/renderer/src/components/ButtonLink.tsx
+++ src/renderer/src/components/ButtonLink.tsx
@@ -1,5 +1,17 @@
-import { Link } from "@tanstack/react-router";
+import { Link, LinkProps } from "@tanstack/react-router";
+import { ComponentProps, deriveClassed } from "@tw-classed/react";
 import { classed } from "~/lib/classed";
 import { Button } from "./Button";
 
-export const ButtonLink = classed(Link, Button);
+// We derive a new component here to correctly type the "to" prop of the Link
+// Strictly typing routes is one of the most important features of tanstack router
+
+const BaseButtonLink = classed(Link, Button);
+
+interface ButtonLinkProps extends ComponentProps<typeof BaseButtonLink> {
+  to: LinkProps["to"];
+}
+
+export const ButtonLink = deriveClassed<typeof BaseButtonLink, ButtonLinkProps>((props) => (
+  <BaseButtonLink {...props} />
+));
