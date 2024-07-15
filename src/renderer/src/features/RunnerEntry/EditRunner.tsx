8522d54ed4e75dc395cfacac5ebfdef90029f000
Jaren Glenn <jarenglenn@gmail.com>
2024-07-15 10:09:47 -0400

    feat: switch between runners when editing

--- src/renderer/src/features/RunnerEntry/EditRunner.tsx
+++ src/renderer/src/features/RunnerEntry/EditRunner.tsx
@@ -3,21 +3,42 @@ import EditIcon from "~/assets/icons/edit.svg?react";
 import { Button, Stack } from "~/components";
 import { Drawer } from "~/components/Drawer";
 import { Runner } from "./useFakeData";
+import { useToasts } from "../Toasts/useToasts";
 
 interface Props {
   runner: Runner;
+  runners: Runner[];
 }
 
 export function EditRunner(props: Props) {
+  const { createToast } = useToasts();
+  const [editingRunner, setEditingRunner] = useState<Runner>(props.runner);
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClose = () => {
     setIsOpen(false);
   };
 
+  const handleChangeRunner = (direction: "previous" | "next") => {
+    const sequence = editingRunner.sequence + (direction === "next" ? 1 : -1);
+    const nextRunner = props.runners.find((runner) => runner.sequence === sequence);
+
+    if (!nextRunner) {
+      createToast({ message: `No ${direction} runner`, type: "warning" });
+      return;
+    }
+
+    setEditingRunner(nextRunner);
+  };
+
   return (
     <>
-      <Button variant="ghost" color="primary" onClick={() => setIsOpen(true)}>
+      <Button
+        variant="ghost"
+        color="primary"
+        onClick={() => setIsOpen(true)}
+        className="p-0 m-0 border-0"
+      >
         <EditIcon width={20} height={20} />
       </Button>
       <Drawer
@@ -29,14 +50,24 @@ export function EditRunner(props: Props) {
       >
         <div className="px-6 pt-8">
           <h1 className="w-full text-4xl font-bold text-center uppercase font-display">
-            Sequence {props.runner.sequence}
+            Sequence {editingRunner.sequence}
           </h1>
 
           <Stack align="center" justify="between" className="w-full">
-            <Button variant="ghost" color="primary" className="text-xl underline">
+            <Button
+              variant="ghost"
+              color="primary"
+              className="text-xl underline"
+              onClick={() => handleChangeRunner("previous")}
+            >
               {"< PREV"}
             </Button>
-            <Button variant="ghost" color="primary" className="text-xl underline">
+            <Button
+              variant="ghost"
+              color="primary"
+              className="text-xl underline"
+              onClick={() => handleChangeRunner("next")}
+            >
               {"NEXT >"}
             </Button>
           </Stack>
