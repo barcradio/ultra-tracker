6fff08f55df7677c8f8aef56fbe1bf2d7046db1a
Jaren Glenn <jarenglenn@gmail.com>
2024-07-22 23:44:21 -0400

    feat: support errors in text inputs

--- src/renderer/src/components/TextInput.tsx
+++ src/renderer/src/components/TextInput.tsx
@@ -1,9 +1,20 @@
 import { ComponentProps, forwardRef } from "react";
+import { FieldError } from "react-hook-form";
+import WarningIcon from "~/assets/icons/warning-circle.svg?react";
 import { classed } from "~/lib/classed";
 import { Stack } from "./Stack";
 
 const Input = classed.input({
-  base: "p-2 w-full rounded-md border-2 outline-none bg-surface-secondary border-on-component text-on-component font-display placeholder:text-component-strong border-component"
+  base: "p-2 w-full rounded-md border-2 outline-none bg-surface-secondary text-on-component font-display placeholder:text-component-strong border-component",
+  variants: {
+    hasError: {
+      true: "border-warning",
+      false: "border-component"
+    }
+  },
+  defaultVariants: {
+    hasError: false
+  }
 });
 
 const Label = classed.label({
@@ -16,15 +27,19 @@ type LabelProps = Omit<ComponentProps<typeof Label>, "for">;
 interface TextInputProps extends InputProps {
   label?: string;
   labelProps?: LabelProps;
+  error?: FieldError;
 }
 
 export const TextInput = forwardRef<HTMLInputElement, TextInputProps>((props, ref) => {
-  const { label, labelProps, ...inputProps } = props;
-
   return (
     <Stack direction="col" className="gap-1 w-full">
-      <Label {...labelProps}>{label}</Label>
-      <Input {...inputProps} ref={ref} />
+      <Stack direction="row" align="center" className="gap-2.5">
+        {props.label && <Label {...props.labelProps}>{props.label}</Label>}
+        {props.error && (
+          <WarningIcon width={20} className="fill-warning animate-in slide-in-from-left" />
+        )}
+      </Stack>
+      <Input {...props} ref={ref} hasError={Boolean(props.error)} />
     </Stack>
   );
 });
