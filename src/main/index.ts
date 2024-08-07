2ec7cf3f216bed0ac77914d653e5e1a7afff8014
Russ Leikis <rleikis@gmail.com>
2024-08-07 17:11:46 -0600

    fix: set a default width and height that is more likely to reflect resolution of even small displays

--- src/main/index.ts
+++ src/main/index.ts
@@ -9,8 +9,8 @@ import settingsIPC from "./ipc/settings-ipc";
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    width: 1440,
+    height: 1080,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === "linux" ? { icon } : {}),
