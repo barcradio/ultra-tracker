e55099eba0cb8e57244e169f79e3ddecc7744092
Jaren Glenn <jarenglenn@gmail.com>
2024-08-24 12:29:45 -0400

    feat(renderer): error handling on useAthletes

--- src/renderer/src/hooks/useAthletes.tsx
+++ src/renderer/src/hooks/useAthletes.tsx
@@ -1,16 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
+import { useToasts } from "~/features/Toasts/useToasts";
+import { DatabaseStatus } from "$shared/enums";
 import { AthleteDB } from "$shared/models";
+import { DatabaseResponse } from "$shared/types";
 import { useIpcRenderer } from "./useIpcRenderer";
 
 export function useAthletes() {
   const ipcRenderer = useIpcRenderer();
+  const { createToast } = useToasts();
 
   return useQuery({
-    queryKey: ["athletes"],
+    queryKey: ["athletes-table"],
     queryFn: async (): Promise<AthleteDB[]> => {
-      const dataset: AthleteDB[] = await ipcRenderer.invoke("get-athletes-table");
-      // TODO: Error handling
-      return dataset;
+      const [data, status, message]: DatabaseResponse<AthleteDB[]> =
+        await ipcRenderer.invoke("get-athletes-table");
+
+      if (status === DatabaseStatus.Error) {
+        createToast({ message, type: "danger" });
+        return [];
+      } else {
+        return data ?? [];
+      }
     }
   });
 }
