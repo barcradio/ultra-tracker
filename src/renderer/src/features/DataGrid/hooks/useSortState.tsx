c294661e4d478472cdb6f5bccc4f04e58de81d3f
Jaren Glenn <jarenglenn@gmail.com>
2024-08-27 12:40:56 -0400

    style(renderer): rework code to avoid ugly line break

--- src/renderer/src/features/DataGrid/hooks/useSortState.tsx
+++ src/renderer/src/features/DataGrid/hooks/useSortState.tsx
@@ -20,11 +20,10 @@ export function useSortState<T extends object>({ initial, columns }: Props<T>) {
   // If this is a problem in the future we can find some other way to get a unique hash.
   const hash = objectHash(columns.map((column) => column.field));
 
-  const [field, setField] = useSessionStorage(`sort-field-${hash}`, initial?.field ?? null);
-  const [ascending, setAscending] = useSessionStorage(
-    `sort-ascending-${hash}`,
-    initial?.ascending ?? true
-  );
+  const initialField = initial?.field ?? null;
+  const initialAscending = initial?.ascending ?? true;
+  const [field, setField] = useSessionStorage(`sort-field-${hash}`, initialField);
+  const [ascending, setAscending] = useSessionStorage(`sort-ascending-${hash}`, initialAscending);
 
   const setSort = (newField: keyof T) => {
     if (field === newField) {
