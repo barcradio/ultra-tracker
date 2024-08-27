0e501539c58084039406e7c8e6ce625e8b0bb6ad
Jaren Glenn <jarenglenn@gmail.com>
2024-08-27 13:15:25 -0400

    docs(renderer): add note about raw html and safety

--- src/renderer/src/components/RenderMarkdown.tsx
+++ src/renderer/src/components/RenderMarkdown.tsx
@@ -6,6 +6,7 @@ type Props = Options & {
 };
 
 export function RenderMarkdown(props: Props) {
+  // NOTE: Raw HTML can be dangerous so we only render it when `trusted` is true.
   const plugins = props.trusted ? [rehypeRaw] : [];
   return <Markdown {...props} rehypePlugins={plugins} className={`markdown ${props.className}`} />;
 }
