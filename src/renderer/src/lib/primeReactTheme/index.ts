1f1cc82c07f4bb190a5e48c020b4846d20685b97
Jaren Glenn <jarenglenn@gmail.com>
2024-08-28 21:52:57 -0400

    refactor: separate primereact component themes

---
+++ src/renderer/src/lib/primeReactTheme/index.ts
@@ -0,0 +1,10 @@
+import { PrimeReactPTOptions } from "primereact/api";
+import { CalendarPT } from "./calendar";
+import { DropdownPT } from "./dropdown";
+import { TooltipPT } from "./tooltip";
+
+export const PrimeReactTheme: PrimeReactPTOptions = {
+  calendar: CalendarPT,
+  tooltip: TooltipPT,
+  dropdown: DropdownPT
+};
