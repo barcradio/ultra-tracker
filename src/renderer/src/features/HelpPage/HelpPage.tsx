84ed267cf0912e3ffad6ca3587afb937785a110f
Jaren Glenn <jarenglenn@gmail.com>
2024-08-29 09:11:57 -0400

    feat: test fix for help page in Windows

--- src/renderer/src/features/HelpPage/HelpPage.tsx
+++ src/renderer/src/features/HelpPage/HelpPage.tsx
@@ -1,12 +1,10 @@
+import getStartedContents from "~/assets/documents/get-started.md?raw";
 import { RenderMarkdown } from "~/components/RenderMarkdown";
-import { useHelpDocumentContents } from "~/hooks/ipc/useHelpContent";
 
 export function HelpPage() {
-  const { data } = useHelpDocumentContents();
-
   return (
     <div className="overflow-y-auto px-4 max-h-inherit bg-component text-on-component">
-      <RenderMarkdown trusted>{data}</RenderMarkdown>
+      <RenderMarkdown trusted>{getStartedContents}</RenderMarkdown>
     </div>
   );
 }
