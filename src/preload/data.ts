44723242ba9bf8c57dbf202cb80ad43ce1883602
Russ Leikis <rleikis@gmail.com>
2024-08-30 15:14:10 -0600

    refactor: updated appSettings initialization

--- src/preload/data.ts
+++ src/preload/data.ts
@@ -22,9 +22,25 @@ export const initializeDefaultAppSettings = () => {
       name: "ultra-marathon-2024"
     },
     station: {
-      name: "5-temple-fork",
       id: 5,
-      entryMode: EntryMode.Normal
+      identifier: "5-temple-fork",
+      name: "Temple Fork",
+      entryMode: EntryMode.Normal,
+      shiftBegin: "00:00:00 Jan 01 2024",
+      shiftEnd: "00:00:00 Jan 01 2024",
+      entrymode: 0,
+      operators: {
+        primary: {
+          name: "Francesco Cossiga",
+          callsign: "I0FCG",
+          phone: ""
+        },
+        secondary: {
+          name: "Carlos Saul Menem",
+          callsign: "LU1SM",
+          phone: ""
+        }
+      }
     },
     incrementalFileIndex: 1
   });
