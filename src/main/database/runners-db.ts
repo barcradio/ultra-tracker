31c19a142d90281272c8562dced7ade7420cae07
Russ Leikis <rleikis@gmail.com>
2024-08-31 12:38:13 -0600

    fix(export): fixed an issue where the first row of each dataset was dropped

--- src/main/database/runners-db.ts
+++ src/main/database/runners-db.ts
@@ -275,26 +275,21 @@ function writeToCSV(filename: string, queryResult, incremental: boolean) {
 
   return new Promise((resolve, reject) => {
     const stream = fs.createWriteStream(filename);
-    let sequence = -1;
 
     // title row
     //const disclaimer = "All times are based off of the system they were recorded on.";
     const headerText = `${eventName},${stationIdentifier}`;
     stream.write(headerText + "\n");
 
+    // header row
+    // index,sent,bibId,timeIn,timeOut,note
     const columnNames = getColumnNamesFromTable("StaEvents");
+    const rowText = `${columnNames[0]},${columnNames[7]},${columnNames[1]},${columnNames[3]},${columnNames[4]},${columnNames[6]}`;
+    stream.write(rowText + "\n");
+
     for (const row of queryResult as RunnerDB[]) {
       let rowText = "";
       const bSent = Boolean(row.sent);
-      // header row
-      // index,sent,bibId,timeIn,timeOut,note
-      if (sequence == -1) {
-        const rowText = `${columnNames[0]},${columnNames[7]},${columnNames[1]},${columnNames[3]},${columnNames[4]},${columnNames[6]}`;
-        stream.write(rowText + "\n");
-        sequence++;
-        continue;
-      }
-
       if (!incremental || (incremental && !bSent)) {
         rowText =
           `${row.index},` +
