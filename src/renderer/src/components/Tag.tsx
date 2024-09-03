811bdcfc2c7bb5dc932cbf8e2f29a22a4de6e172
Jaren Glenn <jarenglenn@gmail.com>
2024-09-03 11:37:05 -0400

    feat(components): basic tag component

---
+++ src/renderer/src/components/Tag.tsx
@@ -0,0 +1,38 @@
+import { MouseEventHandler, ReactNode } from "react";
+import { VariantProps } from "class-variance-authority";
+import { classed } from "~/lib/classed";
+
+type TagVariants = VariantProps<typeof TagWrapper>;
+
+// TODO: Support icons in tag component
+interface TagProps {
+  color?: TagVariants["color"];
+  children: ReactNode;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+const TagWrapper = classed.button(
+  "px-2 text-sm font-medium uppercase rounded-3xl border align-center",
+  {
+    variants: {
+      color: {
+        turquoise: "bg-[#175151] text-[#00FFF0] border-[#00FFF0]",
+        purple: "bg-[#572C7B] text-[#B8B8FF] border-[#B8B8FF]",
+        red: "bg-[#672023] text-[#FFA8A8] border-[#FFA8A8]"
+      }
+    },
+    defaultVariants: {
+      color: "turquoise"
+    }
+  }
+);
+
+export function Tag(props: TagProps) {
+  if (props.children == null) return null;
+
+  return (
+    <TagWrapper disabled={!props.onClick} color={props.color} onClick={props.onClick}>
+      {props.children}
+    </TagWrapper>
+  );
+}
