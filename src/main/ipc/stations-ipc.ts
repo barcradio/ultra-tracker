2a5c6223631fdfc24e33b872d6a508ff43272bdb
Jaren Glenn <jarenglenn@gmail.com>
2024-09-13 00:31:42 -0400

    refactor(main): move footer ipc to stations ipc

--- src/main/ipc/stations-ipc.ts
+++ src/main/ipc/stations-ipc.ts
@@ -1,12 +1,11 @@
 import { ipcMain } from "electron";
-import { StationIdentity } from "../../shared/models";
+import { StationDB, StationIdentity } from "$shared/models";
 import * as dbStations from "../database/stations-db";
 import { Handler } from "../types";
 
 const getStationIdentity: Handler<StationIdentity> = () => {
   return dbStations.GetStationIdentity();
 };
-
-export const initFooterHandlers = () => {
+export const initStationHandlers = () => {
   ipcMain.handle("get-station-identity", getStationIdentity);
 };
