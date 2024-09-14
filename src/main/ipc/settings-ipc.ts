af75dbe99120dfa30cbe26c44bb20adae3441362
Russ Leikis <rleikis@gmail.com>
2024-09-13 21:36:12 -0600

    refactor: remove rfid ipc from settings

--- src/main/ipc/settings-ipc.ts
+++ src/main/ipc/settings-ipc.ts
@@ -2,7 +2,6 @@
 import { ipcMain } from "electron";
 import appSettings from "electron-settings";
 import { resetAppSettings } from "../../preload/data";
-import * as rfid from "../lib/rfid-util";
 import { Handler } from "../types";
 
 // TODO: need to figure out how to provide renderer with app settings.
@@ -15,12 +14,7 @@ const resetSettings: Handler<string> = () => {
   return `${appSettings.file.name}: Reset!`;
 };
 
-const startRFID: Handler<string> = () => {
-  return rfid.initializeRFID();
-};
-
 export const initSettingsHandlers = () => {
   ipcMain.handle("app-settings", getAppSettings);
   ipcMain.handle("reset-app-settings", resetSettings);
-  ipcMain.handle("rfid-initialize", startRFID);
 };
