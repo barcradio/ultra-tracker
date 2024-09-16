0d97fa629e3a52a75d7c1e4e23496200a76a19c0
Russ Leikis <rleikis@gmail.com>
2024-09-15 20:59:41 -0600

    feat: added uber logging function that tries to handle all log types uniformly The Event Log needs to be reworked to allow for more reuse throughout the app.

--- src/main/lib/logger.ts
+++ src/main/lib/logger.ts
@@ -1,6 +1,16 @@
 import path from "path";
 import { app } from "electron";
 import log from "electron-log/main";
+import { logEvent } from "../database/eventLogger-db";
+
+export enum LogLevel {
+  error,
+  warn,
+  info,
+  verbose,
+  debug,
+  silly
+}
 
 // By default, two transports are active: console and file.
 export function initialize() {
@@ -8,12 +18,44 @@ export function initialize() {
   log.transports.file.resolvePathFn = () =>
     path.join(app.getPath("documents"), app.name, ".logs/main.log");
   log.errorHandler.startCatching();
-  //log.transports.console.format = '[{y}-{m}-{d} {h}:{i}:{s}.{ms}] [{level}] {text}';
-  log.info("Log from the main process");
+  log.transports.console.format = "[{iso}] [{level}] [{processType}] {text}";
+  uberLog(LogLevel.info, "startup", "Initializing application: Log from the main process", false);
 }
 
 export function shutdown() {
   log.errorHandler.stopCatching();
 }
 
-//export function logEvent() {}
+export function uberLog(level: LogLevel, scope: string, message: string, sendToEventLog: boolean) {
+  const scopedLog = !scope ? log : log.scope(scope);
+  const logMessage = `${message}`;
+
+  if (sendToEventLog)
+    logEvent(-1, null, null, null, new Date().toISOString(), logMessage, false, false);
+
+  switch (level) {
+    case LogLevel.error:
+      scopedLog.error(logMessage);
+      break;
+
+    case LogLevel.warn:
+      scopedLog.warn(logMessage);
+      break;
+
+    case LogLevel.info:
+      scopedLog.info(logMessage);
+      break;
+
+    case LogLevel.verbose:
+      scopedLog.verbose(logMessage);
+      break;
+
+    case LogLevel.debug:
+      scopedLog.debug(logMessage);
+      break;
+
+    case LogLevel.silly:
+      scopedLog.silly(logMessage);
+      break;
+  }
+}
