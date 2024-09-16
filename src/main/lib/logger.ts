306789fcd46ad3b36f5bd6f28bef8f7fb3b20b83
JordenLuke <jorden.luke@gmail.com>
2024-09-16 06:20:27 -0600

    chore: pointed the console log messages to use the electron log messages console messages are now logged into the console as well as app log

--- src/main/lib/logger.ts
+++ src/main/lib/logger.ts
@@ -19,6 +19,38 @@ export function initialize() {
     path.join(app.getPath("documents"), app.name, ".logs/main.log");
   log.errorHandler.startCatching();
   log.transports.console.format = "[{iso}] [{level}] [{processType}] {text}";
+  // Override console methods to log both to console and electron-log
+  const originalLog = console.log;
+  const originalError = console.error;
+  const originalWarn = console.warn;
+  const originalInfo = console.info;
+  const originalDebug = console.debug;
+
+  console.log = (...args: unknown[]) => {
+    log.log(...args); // Log to electron-log
+    originalLog(...args); // Log to console
+  };
+
+  console.error = (...args: unknown[]) => {
+    log.error(...args); // Log to electron-log
+    originalError(...args); // Log to console
+  };
+
+  console.warn = (...args: unknown[]) => {
+    log.warn(...args); // Log to electron-log
+    originalWarn(...args); // Log to console
+  };
+
+  console.info = (...args: unknown[]) => {
+    log.info(...args); // Log to electron-log
+    originalInfo(...args); // Log to console
+  };
+
+  console.debug = (...args: unknown[]) => {
+    log.debug(...args); // Log to electron-log
+    originalDebug(...args); // Log to console
+  };
+
   uberLog(LogLevel.info, "startup", "Initializing application: Log from the main process", false);
 }
 
