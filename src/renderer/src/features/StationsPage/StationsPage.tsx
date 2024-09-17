3fcafce360bcfbb8cb41fe4389861217eab258a2
Russ Leikis <rleikis@gmail.com>
2024-09-17 00:06:08 -0600

    feat: initial stations datagrid

--- src/renderer/src/features/StationsPage/StationsPage.tsx
+++ src/renderer/src/features/StationsPage/StationsPage.tsx
@@ -5,6 +5,9 @@ import { useStations } from "~/hooks/data/useStations";
 import { StationDB } from "$shared/models";
 import { useIdentityForm } from "./hooks/useIdentityForm";
 import { useStationOperators } from "./hooks/useStationOperators";
+import { ColumnDef, DataGrid } from "../DataGrid";
+import { formatDate } from "~/lib/datetimes";
+import { EntryMode } from "$shared/enums";
 
 function createStationOptions(stations?: StationDB[]) {
   if (!stations) return [];
@@ -27,6 +30,68 @@ export function StationsPage() {
     if (currentOperators) setValue("callsign", currentOperators["primary"]?.callsign);
   }, [currentOperators, setValue]);
 
+  const columns: ColumnDef<StationDB> = [
+    {
+      field: "name",
+      name: "Station"
+    },
+    {
+      field: "location",
+      name: "Lat/Long",
+      valueFn: (station) => {
+        const loc = JSON.parse(station.location);
+        return loc.latitude + ", " + loc.longitude;
+      }
+    },
+    {
+      field: "location",
+      name: "Alt",
+      valueFn: (station) => {
+        const loc = JSON.parse(station.location);
+        return loc.elevation;
+      }
+    },
+    {
+      field: "distance",
+      name: "Dist"
+    },
+    {
+      field: "dropbags",
+      name: "Bags",
+      valueFn: (station) => (station.dropbags ? "Yes" : "No")
+    },
+    {
+      field: "crewaccess",
+      name: "Crew",
+      valueFn: (station) => (station.crewaccess ? "Yes" : "No")
+    },
+    {
+      field: "paceraccess",
+      name: "Pacer",
+      valueFn: (station) => (station.paceraccess ? "Yes" : "No")
+    },
+    {
+      field: "shiftBegin",
+      name: "Open",
+      valueFn: (station) => formatDate(new Date(station.shiftBegin))
+    },
+    {
+      field: "cutofftime",
+      name: "Cutoff",
+      valueFn: (station) => formatDate(new Date(station.cutofftime))
+    },
+    {
+      field: "shiftEnd",
+      name: "Close",
+      valueFn: (station) => formatDate(new Date(station.shiftEnd))
+    },
+    {
+      field: "entrymode",
+      name: "Mode",
+      valueFn: (station) => EntryMode[station.entrymode].toString()
+    }
+  ];
+
   return (
     <div>
       <Stack className="gap-4" align="center" as="form" onSubmit={identityForm.onSubmit}>
@@ -47,6 +112,9 @@ export function StationsPage() {
         />
         <Button className="px-5 py-[6.8px]">Apply</Button>
       </Stack>
+      <div style={{ height: "100vh", paddingTop: "10px" }}>
+        <DataGrid data={stations ?? []} columns={columns} getKey={({ identifier }) => identifier} />
+      </div>
     </div>
   );
 }
