292510ea6c304af4d5ee84768943b8656afb1e84
Jaren Glenn <jarenglenn@gmail.com>
2024-09-20 18:21:58 -0400

    style(store): move schema into constructor for proper typing

--- src/main/lib/store.ts
+++ src/main/lib/store.ts
@@ -1,49 +1,4 @@
-const Store = require("electron-store");
-
-const schema = {
-  initialized: { type: "boolean", default: false },
-  targetLanguage: { type: "string", default: "eng" },
-  incrementalFileIndex: { type: "number", default: 1 },
-  event: {
-    type: "object",
-    properties: {
-      name: { type: "string", default: "" },
-      startline: { type: "string", default: "" },
-      starttime: { type: "string", default: "" },
-      finishline: { type: "string", default: "" },
-      endtime: { type: "string", default: "" }
-    }
-    //required: ["name", "startline", "finishline"]
-  },
-  station: {
-    type: "object",
-    properties: {
-      id: { type: "number", default: 0 },
-      identifier: { type: "string", default: "" },
-      name: { type: "string", default: "" },
-      entryMode: { enum: ["Normal", "Fast", "InOnly", "OutOnly"], default: "Normal" },
-      shiftBegin: { type: "string", default: "00:00:00 Jan 01 2024" },
-      shiftEnd: { type: "string", default: "00:00:00 Jan 01 2024" },
-      entrymode: { type: "number", default: 0 },
-      operators: {
-        type: "object",
-        properties: {
-          primary: {
-            type: "object",
-            properties: {
-              fullname: { type: "string", default: "" },
-              callsign: { type: "string", default: "" },
-              phone: { type: "string", default: "" }
-            }
-          }
-          //required: ["fullname, callsign"]
-        }
-      }
-    }
-    //required: ["primary"]
-  }
-  //required: ["id", "identifier", "name", "entryMode", "operators"]
-};
+import Store from "electron-store";
 
 const defaults = {
   initialized: false,
@@ -68,19 +23,65 @@ const defaults = {
       primary: {
         fullname: "Percy L. Spencer",
         callsign: "W1GBE-SK",
-        phone: "999-999-9999"
+        phone: "999-999-9999",
+        active: false
       },
       secondary: {
         fullname: "Nolan Bushnell",
         callsign: "W7DUK-SK",
-        phone: "999-999-9999"
+        phone: "999-999-9999",
+        active: false
       }
     }
   }
 };
 
 export const appStore = new Store({
-  schema: schema,
+  schema: {
+    initialized: { type: "boolean", default: false },
+    targetLanguage: { type: "string", default: "eng" },
+    incrementalFileIndex: { type: "number", default: 1 },
+    event: {
+      type: "object",
+      properties: {
+        name: { type: "string", default: "" },
+        startline: { type: "string", default: "" },
+        starttime: { type: "string", default: "" },
+        finishline: { type: "string", default: "" },
+        endtime: { type: "string", default: "" }
+      }
+      //required: ["name", "startline, "finishline"]
+    },
+    station: {
+      type: "object",
+      properties: {
+        id: { type: "number", default: 0 },
+        identifier: { type: "string", default: "" },
+        name: { type: "string", default: "" },
+        entryMode: { enum: ["Normal", "Fast", "InOnly", "OutOnly"], default: "Normal" },
+        shiftBegin: { type: "string", default: "00:00:00 Jan 01 2024" },
+        shiftEnd: { type: "string", default: "00:00:00 Jan 01 2024" },
+        entrymode: { type: "number", default: 0 },
+        operators: {
+          type: "object",
+          properties: {
+            primary: {
+              type: "object",
+              properties: {
+                fullname: { type: "string", default: "" },
+                callsign: { type: "string", default: "" },
+                phone: { type: "string", default: "" },
+                active: { type: "boolean", default: false }
+              }
+            }
+            //required: ["fullname, callsign"]
+          }
+        }
+      }
+      //required: ["primary"]
+    }
+    //required: ["id", "identifier", "name", "entryMode", "operators"]
+  },
   defaults: defaults,
   migrations: {},
   clearInvalidConfig: true
