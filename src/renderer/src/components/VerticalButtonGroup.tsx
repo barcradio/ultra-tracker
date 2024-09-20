35bacc688050395e2ec0fc6d5744a224216b9722
Jaren Glenn <jarenglenn@gmail.com>
2024-09-20 00:50:08 -0400

    feat(settings-page): button layouts

--- src/renderer/src/components/VerticalButtonGroup.tsx
+++ src/renderer/src/components/VerticalButtonGroup.tsx
@@ -4,11 +4,15 @@ import { Stack } from "./Stack";
 interface Props {
   label: ReactNode;
   children: ReactNode;
+  className?: string;
 }
 
 export function VerticalButtonGroup(props: Props) {
   return (
-    <Stack direction="col" className="gap-2 bg-component-strong p-4 rounded-xl">
+    <Stack
+      direction="col"
+      className={`gap-2 bg-component-strong p-4 rounded-xl ${props.className}`}
+    >
       {props.label && (
         <span className="font-medium uppercase text-on-surface-strong font-display">
           {props.label}
--- src/renderer/src/features/SettingsPage/SettingsPage.tsx
+++ src/renderer/src/features/SettingsPage/SettingsPage.tsx
@@ -1,84 +1,66 @@
 import { Button, Stack } from "~/components";
+import { VerticalButtonGroup } from "~/components/VerticalButtonGroup";
 import { useSettingsMutations } from "./hooks/useSettingsMutations";
 
 export function SettingsPage() {
   const settingsMutations = useSettingsMutations();
 
   return (
-    <div>
-      <h1>
-        <b>Settings Hub</b>
-      </h1>
-      <div>
-        <Stack direction="row" align="stretch">
-          <Stack direction="col">
-            <b>Station Setup</b>
-            <Button
-              color="primary"
-              size="md"
-              onClick={() => settingsMutations.importStationsFile.mutate()}
-            >
-              Load Stations File
-            </Button>
-            <Button
-              color="primary"
-              size="md"
-              onClick={() => settingsMutations.importAthletesFile.mutate()}
-            >
-              Load Athletes File
-            </Button>
-            <Button
-              color="primary"
-              size="md"
-              onClick={() => settingsMutations.importDNSFile.mutate()}
-            >
-              Load DNS File
-            </Button>
-            <Button
-              color="primary"
-              size="md"
-              onClick={() => settingsMutations.importDNFFile.mutate()}
-            >
-              Load DNF File
-            </Button>
-          </Stack>
-        </Stack>
-      </div>
-      <div>
-        <Stack direction="row" align="stretch">
-          <Stack direction="col">
-            <b>RFID Configuration [Start Line only]</b>
-            <Button
-              color="neutral"
-              variant="outlined"
-              onClick={() => settingsMutations.initializeRfid.mutate()}
-            >
+    <Stack className="w-full h-full bg-component" justify="center" align="center">
+      <Stack justify="center" align="stretch" className="gap-4">
+        <VerticalButtonGroup label="Station Setup">
+          <Button size="wide" onClick={() => settingsMutations.importStationsFile.mutate()}>
+            Load Stations File
+          </Button>
+          <Button size="wide" onClick={() => settingsMutations.importAthletesFile.mutate()}>
+            Load Athletes File
+          </Button>
+          <Button size="wide" onClick={() => settingsMutations.importDNSFile.mutate()}>
+            Load DNS File
+          </Button>
+          <Button size="wide" onClick={() => settingsMutations.importDNFFile.mutate()}>
+            Load DNF File
+          </Button>
+        </VerticalButtonGroup>
+        <Stack direction="col" className="gap-4" justify="stretch">
+          <VerticalButtonGroup
+            className="grow"
+            label={
+              <Stack direction="col">
+                <span className="font-medium">RFID Configuration</span>
+                <span className="text-xs font-medium">(Start Line Only)</span>
+              </Stack>
+            }
+          >
+            <Button size="wide" onClick={() => settingsMutations.initializeRfid.mutate()}>
               Initialize RFID
             </Button>
-          </Stack>
-          <Stack direction="col">
-            <b>App Settings</b>
-            <Button color="warning" variant="outlined" onClick={settingsMutations.resetAppSettings}>
+          </VerticalButtonGroup>
+          <VerticalButtonGroup label="App Settings" className="grow">
+            <Button color="danger" onClick={settingsMutations.resetAppSettings} size="wide">
               Reset App Settings
             </Button>
+          </VerticalButtonGroup>
+        </Stack>
+        <VerticalButtonGroup label="Developer Tools" className="border-2 grow border-danger/30">
+          <Stack direction="col" className="gap-2">
+            <p className="w-80 text-on-surface-strong italic font-display text-sm font-bold mt-2 mb-4">
+              This is a destructive operation! Under most circumstances you should not do this
+              unless instructed do.
+            </p>
+            <Button color="danger" size="wide" onClick={settingsMutations.initializeDatabase}>
+              Recreate Database
+            </Button>
+            <Button
+              color="danger"
+              size="wide"
+              onClick={() => settingsMutations.importRunnerCSVFile.mutate()}
+            >
+              Recover Data from CSV File
+            </Button>
           </Stack>
-        </Stack>
-      </div>
-      <div>
-        <Stack direction="col">
-          <b>Developer Tools</b>
-          <Button color="danger" variant="outlined" onClick={settingsMutations.initializeDatabase}>
-            Destroy & Reinitialize Database
-          </Button>
-          <Button
-            color="danger"
-            variant="outlined"
-            onClick={() => settingsMutations.importRunnerCSVFile.mutate()}
-          >
-            Recover Data from CSV File
-          </Button>
-        </Stack>
-      </div>
-    </div>
+        </VerticalButtonGroup>
+      </Stack>
+    </Stack>
   );
 }
