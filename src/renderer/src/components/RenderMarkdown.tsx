d22a6f80b5c7a3bf8239d36e055d4462a674881c
Russ Leikis <rleikis@gmail.com>
2024-09-20 19:19:54 -0600

    feat: added remark-toc to generate the toc and markdown style links

--- src/renderer/src/components/RenderMarkdown.tsx
+++ src/renderer/src/components/RenderMarkdown.tsx
@@ -1,10 +1,7 @@
 import Markdown, { Options } from "react-markdown";
 import rehypeRaw from "rehype-raw";
 import remarkGfm from "remark-gfm";
-
-// import remarkGitContributors from "remark-git-contributors";
-// import remarkLicense from 'remark-license'
-// import remarkToc from "remark-toc";
+import remarkToc from "remark-toc";
 
 type Props = Options & {
   trusted?: boolean;
@@ -13,11 +10,17 @@ type Props = Options & {
 export function RenderMarkdown(props: Props) {
   // NOTE: Raw HTML can be dangerous so we only render it when `trusted` is true.
   const htmlPlugins = props.trusted ? [rehypeRaw] : [];
-  const markdownPlugins = props.trusted ? [remarkGfm] : [];
+  const markdownPlugins = props.trusted
+    ? [
+        remarkGfm,
+        [remarkToc, { heading: "Table of Contents", maxDepth: 5, tight: true, ordered: true }]
+      ]
+    : [];
 
   return (
     <Markdown
       {...props}
+      // @ts-expect-error some odd quirk with remark plugins and typescript and declaring their options objects
       remarkPlugins={markdownPlugins}
       rehypePlugins={htmlPlugins}
       className={`markdown ${props.className}`}
